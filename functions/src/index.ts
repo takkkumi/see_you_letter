debf6054523662e28a8cfe8c4d5fea3229e91734
takkkumi <nenetyankawaii@gmail.com>
2020-12-10 09:11:41 +0000

    make functions

---
+++ functions/src/index.ts
@@ -0,0 +1,15 @@
+import * as functions from "firebase-functions"
+
+export const testPubSub = functions.pubsub
+	.schedule("every 1 minutes")
+	.onRun((context) => {
+		console.log("毎分実行します")
+		return null
+	})
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
